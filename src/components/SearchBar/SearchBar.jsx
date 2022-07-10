bf6e0123bd268d364573e375d1805b679253b88a
Jordan <jwald3@brockport.edu>
2022-07-10 00:50:35 -0400

    Added clear button to SearchBar

--- src/components/SearchBar/SearchBar.jsx
+++ src/components/SearchBar/SearchBar.jsx
@@ -1,18 +1,28 @@
 import React, { useContext } from 'react'
+import CloseIcon from '@mui/icons-material/Close';
 import { AppContext } from '../../App'
 import './SearchBar.css'
 
 const SearchBar = () => {
 
-    const { setSearchQuery } = useContext(AppContext);
+    const { searchQuery, setSearchQuery } = useContext(AppContext);
 
     const updateQuery = (val) => {
         setSearchQuery(val)
     }
 
+    const clearInput = () => {
+        setSearchQuery("")
+    }
+
     return (
         <div className='search-container'>
-            <input className='search-input' type='text' placeholder='Search for a Pokemon...' onChange={(e) => updateQuery(e.target.value)}/>
+            <div className="inputs">
+                <input className='search-input' type='text' value={searchQuery} placeholder='Search for a Pokemon...' onChange={(e) => updateQuery(e.target.value)}/>
+                <div className="filter-close-icon">
+                    {searchQuery === "" ? "" : <CloseIcon id='clear-btn' onClick={clearInput}/>}
+                </div>
+            </div>
         </div>
     )
 }
