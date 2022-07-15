393af5a2887e11ece39f597384b42c570ce2ecc1
Jordan <jwald3@brockport.edu>
2022-07-15 00:06:19 -0400

    added wiggle effect to PokemonCard images

--- src/components/EvolutionChain/EvolutionChain.jsx
+++ src/components/EvolutionChain/EvolutionChain.jsx
@@ -3,6 +3,12 @@ import { AppContext } from '../../App'
 import axios from 'axios'
 
 const EvolutionChain = () => {
+    /*
+        Planned future functionality for taking the "evolution_chain" property and creating a new element
+        This will return an array of each evolution stage 
+        each evolution stage will be an individual card, they will be contained in one overarching div
+    */
+    
     const { speciesData } = useContext(AppContext);
     const [evolutionURL, setEvolutionURL] = useState();
     const [chainLinks, setChainLinks] = useState([]);
